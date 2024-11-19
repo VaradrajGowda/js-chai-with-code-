e3bce5b58e0a422bd37b90c0704a9b1a04256a86
VaradrajGowda <varadrajgwd143@gmail.com>
2024-11-19 14:30:28 +0000

    onlyopertion.js

---
+++ 01_basics/onlyoperations.js
@@ -0,0 +1,19 @@
+let value = 3
+
+let negvalue = -value 
+
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-3)
+console.log(3*3)
+console.log(3**3)
+console.log(3/5)
+console.log(3%5)
+ 
+let str1 =  "Varad"
+
+let str2 = " Gowda"
+let str3  = str1 + str2 
+console.log(str3)
+
