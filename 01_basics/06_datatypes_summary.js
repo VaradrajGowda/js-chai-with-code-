8a1246bd8a790d365b713ee60d569c8ed307d610
VaradrajGowda <varadrajgwd143@gmail.com>
2024-11-28 15:39:50 +0000

    06

--- 01_basics/06_datatypes_summary.js
+++ 01_basics/06_datatypes_summary.js
@@ -81,3 +81,55 @@ Static typing: Types are checked before the program runs. Less flexible, but cat
 
 
  */
+
+//Non Primitive Datatypes (Array Examples)
+let heroes = ["darshan" ,"Sudeep","Yash"]
+console.log(heroes)
+console.log(heroes[0]) // output darshan
+console.log(heroes[-1]) //  remember the output will be "undefined" in js 
+// because nagative number is always supported in python and not in js 
+// if you want to access the last element from the array in js here is an alternative code 
+console.log(heroes[heroes.length-1]) // output Yash
+
+
+//Non primitive Datatypes (object key value pair )
+
+let profile  = {
+    "name" : "varadraj",
+    "age" : 14
+}
+console.log(profile)
+
+// to acces any particular key value pair 
+
+console.log(profile.name)
+// or 
+console.log(profile["age"])
+
+// to add a new key value pair 
+
+profile["course"]="bscit"
+console.log(profile)
+
+profile["Div"] = "A"
+console.log(profile)
+
+// Non Primitive (Function)
+ let f1  = function(){
+    a =  b  + c 
+    return a 
+ }
+
+b = 12
+c =  34
+console.log(f1()) 
+
+// to know the datatypes of all the Non Primtive datatypes
+
+console.log(typeof f1) // function
+console.log(typeof heroes) // object
+console.log(typeof profile)// object
+
+// for more details refer this link 
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
