0802c685e6ab3aedc148bad194ab9cf2b1212f57
VaradrajGowda <varadrajgwd143@gmail.com>
2024-11-29 15:42:16 +0000

    08

---
+++ 01_basics/08_string.js
@@ -0,0 +1,18 @@
+let name =  "varad"
+let age  = 34
+console.log(name + age + "  Age") // this is not a good practice
+
+// Use Backticks(``)
+//backticks are used to define template literals 
+//inside the template literals we can use doouble and single quotes
+
+let example = `what is 'folish' in the moring "evaing "`
+console.log(example)
+
+//Template literals are used to String interpolations 
+
+//String interpolations are used to insert varibale or expression inside the string with the help of backticks
+
+let friend  =  "rishi"
+let mother  = "sujata"
+console.log(`my frnd name is ${friend} and  my mother name is ${mother}`)
