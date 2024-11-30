6fda1b210b2a177f69b5fffd55be3d11a5fe0cb2
VaradrajGowda <varadrajgwd143@gmail.com>
2024-11-30 08:44:37 +0000

    03

--- 01_basics/08_string.js
+++ 01_basics/08_string.js
@@ -16,3 +16,61 @@ console.log(example)
 let friend  =  "rishi"
 let mother  = "sujata"
 console.log(`my frnd name is ${friend} and  my mother name is ${mother}`)
+
+
+// Another way to declare string 
+// in order to know this advantage paste this below code in browser
+
+let game = new String("Good boy")
+console.log(game)
+
+
+console.log(game.length)// while counting the length of the string the complier will always start from  1 and not from 0 
+
+console.log(game[0]) // output 0 
+console.log(game.toUpperCase()) 
+console.log(game.toLowerCase())
+console.log(game) 
+
+//  remember in the above case we are using primitive datatypes which is a string 
+// so as we are using primitve datatypes we are using stack memory 
+// in stack memory the original data is not changed 
+// due to this  the console.log output is not changed 
+
+//  string methods
+
+let a  = "Bhaiya"
+
+// TO know the length of the sstring 
+console.log(a.length) // 6 (Starts from 1 and not 0 )
+
+// to make the string in captial letters 
+
+console.log(a.toUpperCase())
+
+//toLowerCase() to make the string in small letters 
+
+console.log(a.toLowerCase())
+
+
+//charAt() (when you want to find which character is at a specific position.)
+
+console.log(a.charAt(2))
+
+// indexOf()  (when you what to know the index of the particular character in a string )
+console.log(a.indexOf("y"))
+
+//subsstring() (Extracts part of a string between two indexes.)(Does not support negative indexes.)
+console.log(a.substring(0,2)) // Bh
+
+//Slice() (Extracts part of a string between two indexes or negative positions.)(Supports negative indexes.)
+
+console.log(a.slice(2,-1)) //aiy (here -1 refers to the last elemeny of the string)
+console.log(a.slice(-2 ,-1)) //y
+console.log(a.slice(-4,-2)) //ai
+
+//remember slice and the substring are somewhat similar 
+// only advantage of using the slice over substring 
+//substring does not allow negative values 
+// slice allows positive values 
+
