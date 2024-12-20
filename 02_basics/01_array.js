82bc7d7445cfbf270f9b75ce4fd7607263f97bb7
VaradrajGowda <varadrajgwd143@gmail.com>
2024-12-20 15:28:09 +0000

    ee

---
+++ 02_basics/01_array.js
@@ -0,0 +1,24 @@
+//array
+// defintion -  The Array object, as with arrays in other programming languages, enables storing a col
+// collection of multiple items under a single variable name,
+
+
+//JavaScript arrays are resizable and can contain a mix of different data types.
+
+// JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1, and so on 
+
+// JavaScript array-copy operations create shallow copies.
+
+//shallow copies - A shallow copy of an object is a copy whose properties share the same references
+// As a result, when you change either the source or the copy, you may also cause the other object to change too
+
+
+let array =  [ 1,2,3,4]
+console.log(array[0])
+
+let heroes =  ["yash" , "ganesh"]
+console.log(heroes[1])
+
+// new way to create array 
+let arraynew = new Array(1,2,3,4)
+console.log(arraynew[2])
